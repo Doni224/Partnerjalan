e52c47f879836d25689bd24afb43b95f24d80eca
doni224 <zizam2340@gmail.com>
2022-12-07 10:59:54 +0700

    final

---
+++ src/components/Fasilitas/FasilitasBonus.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <ol>
+          <li v-for="bonus in fasilitasbonus" :key="bonus.id">{{bonus.bonus}}</li>
+      </ol>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  export default {
+      data(){
+      return{
+        fasilitasbonus:[],
+        pathBonus: this.$pathApi
+      }
+    },
+    methods: {
+      setBonus(data) {
+        this.fasilitasbonus = data;
+      },
+    },
+    name: "FasilitasBonus",
+    props: ["bonus"],
+    mounted() {
+      axios
+        .get(this.pathBonus + "api/user/fasilitasuser/bonus", {
+          headers: {
+            "Bypass-Tunnel-Reminder": 1
+          }
+        })
+        .then((response) => this.setBonus(response.data))
+        .catch((error) =>
+          console.log("gagal :", error));
+    },
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ src/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://plain-views-cry-103-191-109-11.loca.lt/",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
